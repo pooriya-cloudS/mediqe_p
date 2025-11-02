3d521f797fca7626a0b284b416047a4f9ff1b988
deblandns <hoseinnysyan1385@gmail.com>
2025-11-03 03:07:06 +0330

    add model of models

--- appointment_mediqe/healthdatas/admin.py
+++ appointment_mediqe/healthdatas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import HealthData
 
 # Register your models here.
+
+
+@admin.register(HealthData)
+class HealthDataAdmin(admin.ModelAdmin):
+    pass
--- appointment_mediqe/healthdatas/models.py
+++ appointment_mediqe/healthdatas/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+# model of health data 
+class HealthData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False, editable=False, verbose_name="Health Data ID")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='health_data', verbose_name="User")
+    ALLERGY = "Allergy"
+    IMMUNIZATION = "Immunization"
+
+    TYPE_CHOICES = [
+        (ALLERGY, "Allergy"),
+        (IMMUNIZATION, "Immunization"),
+    ]
+
+    type = models.CharField(
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=ALLERGY,
+        verbose_name="Type of health data",
+    )
+    name = models.CharField(max_length=255, verbose_name="Name of the health data")
+    reaction_or_provider = models.CharField(max_length=255, verbose_name="Reaction (for Allergy) or Provider (for Immunization)")
+    severity_or_date = models.CharField(max_length=255, verbose_name="Severity (for Allergy) or Date (for Immunization)")
+
+    class Meta:
+        verbose_name = "Health Data"
+        verbose_name_plural = "Health Data Records"
+        ordering = ['-id']
+
+    def __str__(self):
+        return f"{self.type} - {self.name} for {self.user.username}"
